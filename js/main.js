aff14f07bbcbdb09d6921265dc2cc6336587cb24
jibonbiswas007 <33145139+jibonbiswas007@users.noreply.github.com>
2017-12-07 22:27:05 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+        
+        $(".theme-1-select").on('click', function(){
+            $("body").addClass("theme-1").removeClass("theme-2");
+            return false;
+        });
+        
+        $(".theme-2-select").on('click', function(){
+            $("body").addClass("theme-2").removeClass("theme-1");
+            return false;
+        });
+        
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+    
+    
+}(jQuery));
